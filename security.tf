67f93d99c65cea436f635417e8a1acd3bf4745e1
Cleverton Neves <clever.nvs@gmail.com>
2025-10-15 16:28:15 -0300

    feat: Adicionando Security Groups para DB

--- security.tf
+++ security.tf
@@ -218,3 +218,68 @@ resource "aws_network_acl" "devopspro_lab_network_acl_public" {
     Name = "devopspro-lab-network-acl-public"
   }
 }
+
+
+resource "aws_security_group" "devopspro_lab_sg_db" {
+  name        = "devopspro-lab-sg-db"
+  description = "Regras de firewall para as EC2 para o Banco de Dados"
+  vpc_id      = aws_vpc.devopspro_lab_vpc.id
+
+  ingress {
+    description     = "Acesso MongoDB"
+    from_port       = 27017
+    to_port         = 27017
+    protocol        = "tcp"
+    security_groups = [aws_security_group.devopspro_lab_sg_web.id]
+    # cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description = "Acesso SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.devopspro_lab_vpc.cidr_block]
+  }
+
+  egress {
+    description      = "Acesso HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description      = "Acesso HTTPs"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description      = "Acesso DNS"
+    from_port        = 53
+    to_port          = 53
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description      = "Acesso DNS"
+    from_port        = 53
+    to_port          = 53
+    protocol         = "udp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "devopspro-lab-sg-db"
+  }
+}
