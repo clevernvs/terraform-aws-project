dfa64efe1d61f4b8e3b11c64470ba987e75890d6
Cleverton Neves <clever.nvs@gmail.com>
2025-10-14 22:36:10 -0300

    feat: Criando as subnets

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,25 @@
-resource "aws_vpc" "avg_vpc" {
+resource "aws_vpc" "devopspro_lab_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "avg-vpc"
+    Name = "devopspro-lab-vpc"
+  }
+}
+
+resource "aws_subnet" "devopspro_lab_subnet_public" {
+  vpc_id     = aws_vpc.devopspro_lab_vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "devopspro-lab-subnet-public"
+  }
+}
+
+resource "aws_subnet" "devopspro_lab_subnet_public" {
+  vpc_id     = aws_vpc.devopspro_lab_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "devopspro-lab-subnet-public"
   }
 }
