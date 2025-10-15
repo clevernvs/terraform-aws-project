f9eeb9771741623badcf1e0b25aacc531b49e19c
Cleverton Neves <clever.nvs@gmail.com>
2025-10-15 11:15:05 -0300

    feat: Adicionando Network ACL web

--- security.tf
+++ security.tf
@@ -88,3 +88,133 @@ resource "aws_security_group" "devopspro_lab_sg_web" {
     Name = "devopspro-lab-sg-web"
   }
 }
+
+resource "aws_network_acl" "devopspro_lab_network_acl_public" {
+  vpc_id     = aws_vpc.devopspro_lab_vpc.id
+  subnet_ids = [aws_subnet.devopspro_lab_subnet_public.id]
+
+  # Portas HTTP
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # Portas HTTPs
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  # Portas SSH
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 120
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  # Portas Efemeras
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 130
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  # Portas DNS (tcp)
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 140
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 53
+    to_port    = 53
+  }
+
+  # Portas DNS (udp)
+  ingress {
+    protocol   = "udp"
+    rule_no    = 150
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 53
+    to_port    = 53
+  }
+
+  # Portas Efemeras
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  # Portas HTTP
+  egress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # Portas HTTPs
+  egress {
+    protocol   = "tcp"
+    rule_no    = 120
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  # Portas DNS
+  egress {
+    protocol   = "tcp"
+    rule_no    = 130
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 53
+    to_port    = 53
+  }
+
+  # Portas DNS
+  egress {
+    protocol   = "udp"
+    rule_no    = 140
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 53
+    to_port    = 53
+  }
+
+  # Portas SSH
+  egress {
+    protocol   = "tcp"
+    rule_no    = 150
+    action     = "allow"
+    cidr_block = "0.0.0./0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+
+  tags = {
+    Name = "devopspro-lab-network-acl-public"
+  }
+}
