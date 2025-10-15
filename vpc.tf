15394e764c7f9ecb8dde72ab54f9233c9c49f06d
Cleverton Neves <clever.nvs@gmail.com>
2025-10-15 10:10:45 -0300

    feat: Adicionando Route Table

--- vpc.tf
+++ vpc.tf
@@ -54,4 +54,39 @@ resource "aws_nat_gateway" "devopspro_lab_nat_gtw" {
   depends_on = [aws_internet_gateway.devopspro_lab_igw]
 }
 
+resource "aws_route_table" "devopspro_lab_rt_public" {
+  vpc_id = aws_vpc.devopspro_lab_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.devopspro_lab_igw.id
+  }
+
+  tags = {
+    Name = "devopspro-lab-rt-public"
+  }
+}
+
+resource "aws_route_table_association" "devopspro_lab_rt_public_a" {
+  subnet_id      = aws_subnet.devopspro_lab_subnet_public.id
+  route_table_id = aws_route_table.devopspro_lab_rt_public.id
+}
+
+resource "aws_route_table" "devopspro_lab_rt_private" {
+  vpc_id = aws_vpc.devopspro_lab_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_internet_gateway.devopspro_lab_nat_gtw.id
+  }
+
+  tags = {
+    Name = "devopspro-lab-rt-private"
+  }
+}
+
+resource "aws_route_table_association" "devopspro_lab_rt_private_a" {
+  subnet_id      = aws_subnet.devopspro_lab_subnet_private.id
+  route_table_id = aws_route_table.devopspro_lab_rt_private.id
+}
 
